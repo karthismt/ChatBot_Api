75ecba3a5343329aed2db634872a22a3142afcc0
Karthi <karthikeyan.d@ivymobility.com>
2020-06-20 20:21:01 +0530

    Added Web Api

---
+++ ChatBotApi/ChatbotContext.cs
@@ -0,0 +1,28 @@
+﻿
+using ChatBotApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ChatBotApi
+{
+    public class ChatbotContext :DbContext
+    {
+        public ChatbotContext()
+            : base("DB_Context")
+        {
+            Database.SetInitializer<ChatbotContext>(null);//Disable initializer
+        }
+
+        public DbSet<ChatBot> ChatBot { get; set; }
+        public DbSet<ContactInfromation> ContactInfromation { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ChatBotApi/Controllers/QuestionsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatBotApi.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        //
+        // GET: /Questions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult GetAllQuestionDetails()
+        {
+            ChatbotContext cb = new ChatbotContext();
+            var chatbotResult = (from a in cb.ChatBot
+                                 select new
+                                 {
+                                     a.ContactId,
+                                     a.Desc,
+                                     a.IsHeader,
+                                     a.LevelId,
+                                     a.ParentId,
+                                     a.QID
+                                 }).ToList();
+
+            return Json(new { QuestionMaster = chatbotResult }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult GetAllContactDetails()
+        {
+            ChatbotContext cb = new ChatbotContext();
+            var chatbotResult = (from a in cb.ContactInfromation
+                                 select new
+                                 {
+                                     a.id,
+                                     a.name,
+                                     a.phoneNumber,
+                                     a.website,
+                                     a.email,
+                                     a.Address,
+
+                                 }).ToList();
+
+            return Json(new { ContactInfromation = chatbotResult }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
